c3cf16c778fe5fa23a5c41e3cdf8dd035b6683e5
pahlavaubivca <pahlavaubivca@gmail.com>
2017-10-15 16:33:21 +0300

    fire direction done add enemy script

---
+++ Assets/scripts/main/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.name == "bullet"){
+			Debug.Log("collision");
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
