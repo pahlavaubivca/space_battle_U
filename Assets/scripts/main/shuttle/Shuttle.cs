60846936a9a99b23c5e030ba85ccdf6546f4fe66
pahlavaubivca <pahlavaubivca@gmail.com>
2017-11-08 23:58:11 +0200

    shuttle move by rigidBody addForce trying limit speed

--- Assets/scripts/main/shuttle/Shuttle.cs
+++ Assets/scripts/main/shuttle/Shuttle.cs
@@ -7,17 +7,20 @@ namespace main{
     public class Shuttle : MonoBehaviour{
         public bool ThisIsShuttle = true;
         private Fire _fire;
-        private float _limit = 2.5f;
+        private float _limit = 0.5f;
+        private float maxSpeed = 0.01f;
         private Vector3 _mousePosition;
         private double _dist;
         private GameObject shuttlePosition;
-        private Rigidbody2D _bodyRigid;
-        
+        private Rigidbody2D _rigidBody;
+
         void Start(){
             _fire = this.GetComponent<Fire>();
             shuttlePosition = GameObject.Find("shuttlePosition");
-            _bodyRigid = gameObject.GetComponent<Rigidbody2D>();
+            _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+//            _rigidBody.drag = 0.0001f;
         }
+
         void Update(){
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.transform.eulerAngles = new Vector3(0, 0,
@@ -25,8 +28,8 @@ namespace main{
                 Mathf.Rad2Deg);
 //            Shuttle asd = gameObject.GetComponent<Shuttle>();
 //            asd.GetComponent<Rigidbody2D>().transform.position = new Vector3(0,0,0);
-          //  transform.transform.position = new Vector3(transform.position.x,transform.position.y,0);
-            
+            //  transform.transform.position = new Vector3(transform.position.x,transform.position.y,0);
+
             if (Input.GetMouseButtonDown(0)){
                 _fire.CalculateStart();
             }
@@ -40,14 +43,36 @@ namespace main{
             if (_dist > _limit){
                 koef = _limit * (_limit / koef);
             }
-            transform.position = Vector2.Lerp(transform.position, _mousePosition, koef / 3000);
-            // Debug.Log(_bodyRigid.velocity.magnitude);
-//            if (_bodyRigid.velocity.x > 0.1f || _bodyRigid.velocity.y > 0.1f){
-//                _bodyRigid.velocity = new Vector2(1f,1f);
-//                Debug.Log(_bodyRigid.velocity.magnitude);
+            //transform.position = Vector2.Lerp(transform.position, _mousePosition, koef / 300);
+//            if (_rigidBody.velocity.magnitude > _limit){
+//                _rigidBody.velocity = _rigidBody.velocity.normalized * _limit;
+//            } else{
+//                _rigidBody.AddForce(_mousePosition * koef / 30);
+//                _rigidBody.velocity = Vector3.ClampMagnitude(_rigidBody.velocity, _limit);
 //            }
+
+            _rigidBody.AddForce(_mousePosition * koef / 300, ForceMode2D.Force);
+            if (Mathf.Abs(_rigidBody.velocity.x) > 0.01f){
+                Debug.Log("x limit - " + _rigidBody.velocity.x);
+                _rigidBody.velocity = new Vector2(_rigidBody.velocity.x < 0 ? maxSpeed * -1 : maxSpeed, _rigidBody.velocity.y);
+            }
+            if (Mathf.Abs(_rigidBody.velocity.y) > 0.01f){
+                _rigidBody.velocity = new Vector2(_rigidBody.velocity.x , _rigidBody.velocity.y < 0 ? maxSpeed * -1 : maxSpeed);
+            }
+//            if (Mathf.Abs(_rigidBody.velocity.x) + Mathf.Abs(_rigidBody.velocity.y) > 0.01f){
+//                //_rigidBody.velocity = _rigidBody.velocity * koef / 300;
+//                _rigidBody.velocity = new Vector2(transform.position.x < 0 ? maxSpeed * -1 : maxSpeed,
+//                    transform.position.y < 0 ? maxSpeed * -1 : maxSpeed);
+//                //_rigidBody.velocity = Vector3.Normalize(_rigidBody.velocity) * Mathf.Sqrt(0.001f);
+//                Debug.Log("trying to normalize, magnitude - " + _rigidBody.velocity.magnitude);
+//            } 
+
+            //_rigidBody.velocity = Vector3.ClampMagnitude(_rigidBody.velocity, _limit);
+//            Debug.Log(_rigidBody.velocity.magnitude);
+
             shuttlePosition.transform.position = transform.position;
         }
+
         private float distance(float x0, float x1, float y0, float y1){
             return (float) Math.Sqrt(Math.Pow(x0 - x1, 2) +
                                      Math.Pow(y0 - y1, 2));
